09b92eac8083c1e3a39ed252c694334f33d0e9e9
amaitland <amaitland@users.noreply.github.com>
2016-01-13 10:53:46 +1000

    For simplicities sake strip out all the MVVM parts and simplify the example down to the bare minimum (Code is declared in the MainWindow now)

---
+++ CefSharp.MinimalExample.Wpf/Binding/TitleConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CefSharp.MinimalExample.Wpf.Binding
+{
+    public class TitleConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            return "CefSharp.MinimalExample.Wpf - " + (value ?? "No Title Specified");
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Windows.Data.Binding.DoNothing;
+        }
+    }
+}
