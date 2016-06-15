5150ce34eff1fc33b0e90d86816ad34866ffd5f0
amaitland <amaitland@users.noreply.github.com>
2016-06-16 08:14:16 +1000

    Disable GPU on windows 7 https://github.com/cefsharp/CefSharp/issues/1634

--- CefSharp.MinimalExample.OffScreen/Program.cs
+++ CefSharp.MinimalExample.OffScreen/Program.cs
@@ -24,6 +24,14 @@ namespace CefSharp.MinimalExample.OffScreen
 
             var settings = new CefSettings();
 
+            var osVersion = Environment.OSVersion;
+            //Disable GPU for Windows 7
+            if (osVersion.Version.Major == 6 && osVersion.Version.Minor == 1)
+            {
+                // Disable GPU in WPF and Offscreen examples until #1634 has been resolved
+                settings.CefCommandLineArgs.Add("disable-gpu", "1");
+            }
+
             //Perform dependency check to make sure all relevant resources are in our output directory.
             Cef.Initialize(settings, shutdownOnProcessExit: false, performDependencyCheck: true);
 
