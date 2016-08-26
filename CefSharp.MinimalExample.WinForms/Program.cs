e7e919c21a0a7a1025529af655ae1dbf4bf729f1
amaitland <amaitland@users.noreply.github.com>
2016-08-26 20:38:26 +1000

    Demo AnyCpu support as part of the WinForms project - Add assembly resolver

--- CefSharp.MinimalExample.WinForms/Program.cs
+++ CefSharp.MinimalExample.WinForms/Program.cs
@@ -3,6 +3,9 @@
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
 using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows.Forms;
 
 namespace CefSharp.MinimalExample.WinForms
@@ -12,11 +15,43 @@ namespace CefSharp.MinimalExample.WinForms
         [STAThread]
         public static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
+
+            LoadApp();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void LoadApp()
+        {
             //Perform dependency check to make sure all relevant resources are in our output directory.
-            Cef.Initialize(new CefSettings(), shutdownOnProcessExit:false, performDependencyCheck:true);
+            var settings = new CefSettings();
+
+            settings.BrowserSubprocessPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                   Environment.Is64BitProcess ? "x64" : "x86",
+                                                   "CefSharp.BrowserSubprocess.exe");
+
+            Cef.Initialize(settings, shutdownOnProcessExit: false, performDependencyCheck: false);
 
             var browser = new BrowserForm();
             Application.Run(browser);
+        }
+
+        // Will attempt to load missing assembly from either x86 or x64 subdir
+        private static Assembly Resolver(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.StartsWith("CefSharp"))
+            {
+                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
+                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                       Environment.Is64BitProcess ? "x64" : "x86",
+                                                       assemblyName);
+
+                return File.Exists(archSpecificPath)
+                           ? Assembly.LoadFile(archSpecificPath)
+                           : null;
+            }
+
+            return null;
         }
     }
 }
