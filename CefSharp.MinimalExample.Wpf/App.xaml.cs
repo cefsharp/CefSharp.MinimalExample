509948fb4bb374cb0b8c90188c3f002f00486e75
amaitland <alex@maitlandcomputing.com.au>
2016-10-12 13:16:47 +1000

    No longer need to disable GPU acceleration in 53.0.0

--- CefSharp.MinimalExample.Wpf/App.xaml.cs
+++ CefSharp.MinimalExample.Wpf/App.xaml.cs
@@ -6,19 +6,13 @@ namespace CefSharp.MinimalExample.Wpf
 {
     public partial class App : Application
     {
+        private DispatcherTimer messageLoopTimer;
+
         public App()
         {
             //Perform dependency check to make sure all relevant resources are in our output directory.
             var settings = new CefSettings();
             settings.MultiThreadedMessageLoop = false;
-
-            var osVersion = Environment.OSVersion;
-            //Disable GPU for Windows 7
-            if (osVersion.Version.Major == 6 && osVersion.Version.Minor == 1)
-            {
-                // Disable GPU in WPF and Offscreen examples until #1634 has been resolved
-                settings.CefCommandLineArgs.Add("disable-gpu", "1");
-            }
 
             Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
         }
@@ -28,14 +22,25 @@ namespace CefSharp.MinimalExample.Wpf
             base.OnStartup(startupEventArgs);
 
             //Run DoMessageLoopWork 60 times per second - this is the simplest message loop integration
-            var timer = new DispatcherTimer
+            messageLoopTimer = new DispatcherTimer
             (
                 TimeSpan.FromMilliseconds(1000 / 60),
                 DispatcherPriority.Render,
                 (s, e) => Cef.DoMessageLoopWork(),
                 Dispatcher
             );
-            timer.Start();
+            messageLoopTimer.Start();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (messageLoopTimer != null)
+            {
+                messageLoopTimer.Stop();
+                messageLoopTimer = null;
+            }
+
+            base.OnExit(e);
         }
     }
 }
