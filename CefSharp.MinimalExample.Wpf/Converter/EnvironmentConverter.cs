2b226c2114eb1c00cee30f56dde74c95c75bd858
Nikhil Agrawal <nikhilagrawal.net@outlook.com>
2017-04-06 02:10:14 +0530

    Added Hover Link, ChromiumVersion, CefVersion, CefSharpVersion, ProcessInfo in Status Bar. Added Back, Forward, Print and View souce buttons and Addressbar.

---
+++ CefSharp.MinimalExample.Wpf/Converter/EnvironmentConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace CefSharp.MinimalExample.Wpf.Converter
+{
+    class EnvironmentConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Environment.Is64BitProcess ? "x64" : "x86";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Windows.Data.Binding.DoNothing;
+        }
+    }
+}
