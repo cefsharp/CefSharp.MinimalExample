d17b5d8215bbe728770908ed67ec568093cf0847
amaitland <alex@maitlandcomputing.com.au>
2018-03-22 16:04:39 +1000

    WinForms Example - Demo Prefer32bit for AnyCPU

--- CefSharp.MinimalExample.WinForms/Program.cs
+++ CefSharp.MinimalExample.WinForms/Program.cs
@@ -15,43 +15,16 @@ namespace CefSharp.MinimalExample.WinForms
         [STAThread]
         public static void Main()
         {
-            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
-
-            LoadApp();
-        }
-
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        private static void LoadApp()
-        {
             //Perform dependency check to make sure all relevant resources are in our output directory.
             var settings = new CefSettings();
 
             settings.BrowserSubprocessPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-                                                   Environment.Is64BitProcess ? "x64" : "x86",
-                                                   "CefSharp.BrowserSubprocess.exe");
+                                                   "x86", "CefSharp.BrowserSubprocess.exe");
 
             Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
 
             var browser = new BrowserForm();
             Application.Run(browser);
-        }
-
-        // Will attempt to load missing assembly from either x86 or x64 subdir
-        private static Assembly Resolver(object sender, ResolveEventArgs args)
-        {
-            if (args.Name.StartsWith("CefSharp"))
-            {
-                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
-                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-                                                       Environment.Is64BitProcess ? "x64" : "x86",
-                                                       assemblyName);
-
-                return File.Exists(archSpecificPath)
-                           ? Assembly.LoadFile(archSpecificPath)
-                           : null;
-            }
-
-            return null;
         }
     }
 }
