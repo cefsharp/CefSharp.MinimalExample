05dc8ad9278584c0d5f8cf7d6d500fd1358a173d
amaitland <amaitland@users.noreply.github.com>
2019-03-20 21:06:51 +1000

    Demo using your application exe as the BrowserSubprocess
    - Add a reference to CefSharp.BrowserSubprocess.Core to your project file
    - Updated Program.cs, basically integrate the standard CefSharp.BrowserSubprocess.exe code directly
    - Add example RenderProcessHandler
    
    Demos #2697

--- CefSharp.MinimalExample.WinForms/Program.cs
+++ CefSharp.MinimalExample.WinForms/Program.cs
@@ -1,41 +1,127 @@
-﻿// Copyright © 2010-2015 The CefSharp Authors. All rights reserved.
+﻿// Copyright © 2010-2019 The CefSharp Authors. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
 using CefSharp.WinForms;
 using System;
 using System.IO;
+using System.Diagnostics;
 using System.Windows.Forms;
+using CefSharp.Internals;
+using System.Threading.Tasks;
+using CefSharp.RenderProcess;
+using CefSharp.BrowserSubprocess;
+using CefSharp.MinimalExample.WinForms.RenderProcess;
 
 namespace CefSharp.MinimalExample.WinForms
 {
+    /// <summary>
+    /// Demos integrating the browser subprocess into your application executable
+    /// </summary>
     public class Program
     {
         [STAThread]
-        public static void Main()
+        public static int Main(string[] args)
         {
-            //Monitor parent process exit and close subprocesses if parent process exits first
-            //This will at some point in the future becomes the default
-            CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
-
             //For Windows 7 and above, best to include relevant app.manifest entries as well
             Cef.EnableHighDPISupport();
 
-            var settings = new CefSettings()
+            var type = args.GetArgumentValue(CefSharpArguments.SubProcessTypeArgument);
+
+            if(type == null)
             {
-                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
-                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
-            };
+                var settings = new CefSettings()
+                {
+                    //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                    CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache"),
+                    //Using your own executable as the subprocess requires
+                    BrowserSubprocessPath = Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), "CefSharp.MinimalExample.WinForms.exe")
+                };
 
-            //Example of setting a command line argument
-            //Enables WebRTC
-            settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+                //Example of setting a command line argument
+                //Enables WebRTC
+                settings.CefCommandLineArgs.Add("enable-media-stream", "1");
 
-            //Perform dependency check to make sure all relevant resources are in our output directory.
-            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+                //Enable this to debug the BrowserProcess
+                //A dialog will popup allowing you to attach the debugger to the process
+                //settings.CefCommandLineArgs.Add("renderer-startup-dialog", "1");
 
-            var browser = new BrowserForm();
-            Application.Run(browser);
+                //Perform dependency check to make sure all relevant resources are in our output directory.
+                Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+
+                var browser = new BrowserForm();
+                Application.Run(browser);
+
+                return 0;
+            }
+
+            return CustomBrowserProcessCode(type, args);
+        }
+
+        /// <summary>
+        /// Custom BrowserSubProcess handling code goes here
+        /// </summary>
+        /// <returns>error code</returns>
+        private static int CustomBrowserProcessCode(string type, string[] args)
+        {
+            int result;
+
+            Debug.WriteLine("BrowserSubprocess starting up with command line: " + string.Join("\n", args));
+
+            var parentProcessId = -1;
+
+            // The Crashpad Handler doesn't have any HostProcessIdArgument, so we must not try to
+            // parse it lest we want an ArgumentNullException.
+            if (type != "crashpad-handler")
+            {
+                parentProcessId = int.Parse(args.GetArgumentValue(CefSharpArguments.HostProcessIdArgument));
+                if (args.HasArgument(CefSharpArguments.ExitIfParentProcessClosed))
+                {
+                    Task.Factory.StartNew(() => AwaitParentProcessExit(parentProcessId), TaskCreationOptions.LongRunning);
+                }
+            }
+
+            // Use our custom subProcess provides features like EvaluateJavascript
+            if (type == "renderer")
+            {
+                //Add your own custom implementation of IRenderProcessHandler here
+                IRenderProcessHandler handler = new RenderProcessHandler();
+                var wcfEnabled = args.HasArgument(CefSharpArguments.WcfEnabledArgument);
+                var subProcess = wcfEnabled ? new WcfEnabledSubProcess(parentProcessId, handler, args) : new SubProcess(handler, args);
+
+                using (subProcess)
+                {
+                    result = subProcess.Run();
+                }
+            }
+            else
+            {
+                result = SubProcess.ExecuteProcess();
+            }
+
+            Debug.WriteLine("BrowserSubprocess shutting down.");
+
+            return result;
+        }
+
+        private static async void AwaitParentProcessExit(int parentProcessId)
+        {
+            try
+            {
+                var parentProcess = Process.GetProcessById(parentProcessId);
+                parentProcess.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                //main process probably died already
+                Debug.WriteLine(e);
+            }
+
+            await Task.Delay(1000); //wait a bit before exiting
+
+            Debug.WriteLine("BrowserSubprocess shutting down forcibly.");
+
+            Environment.Exit(0);
         }
     }
 }
---
+++ CefSharp.MinimalExample.WinForms/RenderProcess/RenderProcessHandler.cs
@@ -0,0 +1,26 @@
+﻿// Copyright © 2010-2019 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using CefSharp.RenderProcess;
+
+namespace CefSharp.MinimalExample.WinForms.RenderProcess
+{
+    public class RenderProcessHandler : IRenderProcessHandler
+    {
+        void IRenderProcessHandler.OnContextCreated(IBrowser browser, IFrame frame, IV8Context context)
+        {
+            //TODO: browser and frame are currently null as there is no C++ wrapper in the render process currently.
+            V8Exception ex;
+            if (!context.Execute("Object.freeze(console)", "about:blank", 1, out ex))
+            {
+                //TODO: Do something with the exception
+            }
+        }
+
+        void IRenderProcessHandler.OnContextReleased(IBrowser browser, IFrame frame, IV8Context context)
+        {
+            
+        }
+    }
+}
