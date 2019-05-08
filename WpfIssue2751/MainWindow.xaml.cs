47a16f95e89353d8ad182e03ca9f28bab2e70be4
Liang Gao <gaoliang@dihmed.com>
2019-05-08 09:10:47 +0800

    .

--- WpfIssue2751/MainWindow.xaml.cs
+++ WpfIssue2751/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using CefSharp.Wpf;
 using CefSharp;
 using System.Drawing;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 
 namespace WpfIssue2751
@@ -33,15 +34,19 @@ namespace WpfIssue2751
 
             DataContext = this;
 
-            MyTitle = "Show CefSharp.Wpf Issue 2751";
+            MyTitle = "CefZoom";
 
             SizeChanged += OnSizeChanged;
 
             Loaded += OnLoad;
 
+            //myScrollViewer.
+
             browser.LoadingStateChanged += OnBrowserLoaded;
             browser.SizeChanged += OnBrowserSizeChanged;
-            browser.Paint += OnBrowserPaint;
+
+            //CurrentPageIndex = 0;
+            //TotalPageCount = 0;
         }
 
         public double ScrollViewHeight { get; } = 474;
@@ -49,26 +54,63 @@ namespace WpfIssue2751
 
         private void OnLoad(object sender, RoutedEventArgs e)
         {
-            AddLog("OnLoad");
+            AddLog($"OnLoad() IsBrowserLoaded={IsBrowserLoaded}");
         }
 
         private void OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
-            //AddLog("OnSizeChanged");
+            AddLog($"OnSizeChanged() IsBrowserLoaded={IsBrowserLoaded}");
 
             this.browser.Height = ScrollViewHeight;
             this.browser.Width = ScrollViewWidth;
         }
 
-        private void OnBrowserPaint(object sender, PaintEventArgs e)
+        private void OnScrollViewerLayoutUpdated(object sender, EventArgs e)
         {
-            //AddLog("OnBrowserPaint");
-        }
-        private void OnBrowserSizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            AddLog("OnBrowserSizeChanged");
+            AddLog("OnScrollViewerLayoutUpdated");
+
         }
 
+        async private void OnBrowserSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (IsBrowserLoaded)
+            {
+                AddLog($"OnBrowserSizeChanged() IsResizing={IsResizing}");
+
+                //first time
+                if (!HasResized)
+                {
+                    HasResized = true;
+                    CurrentPageIndex = 1;
+                    PendingZoomIndex = 1;
+                }
+                else
+                {
+                    ;
+                }
+
+                AddLog($"myScrollViewer.UpdateLayout()-before");
+                myScrollViewer.UpdateLayout();
+                for(int i = 1; i <= 10; i++)
+                {
+                    await Task.Delay(100);
+                    AddLog($"myScrollViewer.UpdateLayout()-For Loop{i*100}");
+                }
+
+                //await Task.Delay(1000);
+
+                ActualZoomIndex = PendingZoomIndex;
+                CurrentZoomIndex = ActualZoomIndex;
+
+                GotoPage(CurrentPageIndex);
+
+                IsResizing = false;
+            }
+        }
+
+        public bool IsBrowserLoaded { get; set; }
+        public bool HasResized { get; set; }
+        public bool IsResizing { get; set; }
         async private void OnBrowserLoaded(object sender, LoadingStateChangedEventArgs e)
         {
             if (!e.IsLoading)
@@ -77,38 +119,32 @@ namespace WpfIssue2751
 
                 await Dispatcher.Invoke(async () =>
                 {
+                    IsBrowserLoaded = true;
+                    HasResized = false;
+
                     BodyHeight = await GetBodyHeight();
                     BodyWidth = await GetBodyWidth();
                     HtmlPageHeight = await GetPageHeight();
                     HtmlPageWidth = BodyWidth;
 
                     TotalPageCount = await GetPageCount();
+                    //CurrentPageIndex = 1;
 
-                    SetZoom("100");
+                    IsResizing = false;
+
+                    SetZoom(1);//100
                 });
             }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        private string myTitle;
-
-        public string MyTitle
-        {
-            get { return myTitle; }
-            set
-            {
-                myTitle = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MyTitle)));
-            }
-        }
 
         private void AddLog(string info)
         {
-            string msg = $"{DateTime.Now.ToString("HH:mm:ss.fff")} - {info} -ActualWidth={browser.ActualWidth},ActHeight={browser.ActualHeight} ";
-
+            //string msg = $"{DateTime.Now.ToString("HH:mm:ss.fff")} - {info} -ActualWidth={browser.ActualWidth},ActHeight={browser.ActualHeight} ,ScrollHeight={myScrollViewer.ExtentHeight}";
             Dispatcher.Invoke(() =>
             {
+                string msg = $"{DateTime.Now.ToString("HH:mm:ss.fff")} - {info} -browserActHeight={browser.ActualHeight} ,ScrollExtentHeight={myScrollViewer.ExtentHeight}";
                 this.LogCollection.Insert(0, msg);
             });
         }
@@ -125,27 +161,27 @@ namespace WpfIssue2751
             }
         }
 
-        private int pageCount;
+        private int totalPageCount;
 
         public int TotalPageCount
         {
-            get { return pageCount; }
+            get { return totalPageCount; }
             set
             {
-                pageCount = value;
+                totalPageCount = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TotalPageCount)));
             }
         }
 
-        private int currentPage;
+        private int currentPageIndex;
 
-        public int CurrentPage
+        public int CurrentPageIndex
         {
-            get { return currentPage; }
+            get { return currentPageIndex; }
             set
             {
-                currentPage = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentPage)));
+                currentPageIndex = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentPageIndex)));
             }
         }
 
@@ -199,6 +235,189 @@ namespace WpfIssue2751
             }
 
             return result;
+        }
+
+        private void LoadHtmlFile(string filename)
+        {
+            string html = File.ReadAllText(filename);
+            Dispatcher.Invoke(() =>
+            {
+                IsBrowserLoaded = false;
+                browser.LoadHtml(html, "http://ReportingControl");
+
+            });
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = "debug-big.html";
+            LoadHtmlFile(filename);
+        }
+
+        private void DownBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentPageIndex > 1)
+            {
+                CurrentPageIndex--;
+                GotoPage(CurrentPageIndex);
+            }
+        }
+
+        private void UpBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentPageIndex < (TotalPageCount))
+            {
+                CurrentPageIndex++;
+                GotoPage(CurrentPageIndex);
+            }
+        }
+
+        private void PART_VerticalScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //AddLog("PART_VerticalScrollBar_ValueChanged");                       
+            CurrentPageIndex = GetCurrtentPage();
+            //update
+        }
+
+        public double CurrentZoomPercent { get; set; }
+        public double CurrentZoomLevel { get; set; }
+
+        private double GetBrowserZoomPercent(int zoomIndex)
+        {
+            switch (zoomIndex)
+            {
+                case 0:
+                    //50%
+                    return (ScrollViewHeight+10) / HtmlPageHeight;
+                case 2:
+                    //125%
+                    return ScrollViewWidth*1.25 / HtmlPageWidth;
+                case 1:
+                default:
+                    return ScrollViewWidth / HtmlPageWidth;
+            }
+        }
+
+        private double GetBrowserZoomLevel(double zoomPercent)
+        {
+            double result = Math.Log(zoomPercent) / Math.Log(1.2);
+            //a little bigger
+            return Math.Ceiling(result * 1000) / 1000;
+        }
+
+        private void SetZoom(int zoomIndex)
+        {
+            string message = IsResizing ? "Deny" : "Resizing";
+            AddLog($"SetZoom({zoomIndex}) {message}");
+
+            ////browser.Height = BodyHeight;
+            //browser.Height = ScreenHeight*9.103;
+            //browser.Width = BodyWidth;
+
+            //browser.InvalidateVisual();
+            //int currentPageIndex = GetCurrtentPage();
+
+            //CurrentPage = GetCurrtentPage();
+            if (!IsResizing)
+            {
+                IsResizing = true;
+
+                if (HasResized)
+                {
+                    if (ActualZoomIndex == zoomIndex)
+                    {
+                        AddLog($"SetZoom({zoomIndex}), same , not resizing...");
+
+                        IsResizing = false;
+                        CurrentZoomIndex = ActualZoomIndex;
+                        return;
+                    }
+                }
+
+                PendingZoomIndex = zoomIndex;
+
+                CurrentZoomPercent = GetBrowserZoomPercent(zoomIndex);
+                CurrentZoomLevel = GetBrowserZoomLevel(CurrentZoomPercent);
+
+                ZoomedPageHeight = HtmlPageHeight * CurrentZoomPercent;
+                ZoomedPageWidth = HtmlPageWidth * CurrentZoomPercent;
+
+
+                Dispatcher.Invoke(() =>
+                {
+                    //AddLog($"browser.SetZoomLevel() - before");
+
+                    browser.SetZoomLevel(CurrentZoomLevel);
+                    //add a little more
+                    //browser.Height = TotalPageCount * ZoomedPageHeight + 0.1;
+                    browser.Height = ZoomedPageHeight * TotalPageCount;
+
+                    //browser.Width = ZoomedPageWidth > ScrollViewWidth ? ZoomedPageWidth : ScrollViewWidth;
+                    browser.Width = ZoomedPageWidth;
+
+                   // AddLog($"browser.InvalidateVisual() - before");
+
+                    //update browser layout and render it
+                    //browser.InvalidateMeasure();//This method calls InvalidateArrange internally.
+                    browser.InvalidateVisual();//This method calls InvalidateArrange internally.
+                    //AddLog($"browser.InvalidateVisual() - after");
+                });
+            }
+
+            //GotoPage(currentPageIndex);
+        }
+
+        public void GotoPage(int currentPageIndex)
+        {
+            int index = currentPageIndex;
+            if (index <= 1) { index = 1; }
+            if (index >= TotalPageCount) { index = TotalPageCount; }
+
+            //add a little more
+            double verticalOffset = (index - 1) * ZoomedPageHeight + 0.1 * CurrentZoomPercent;
+            AddLog($"GotoPage({index}), {verticalOffset}, ZoomedHeight={ZoomedPageHeight}");
+
+            myScrollViewer.ScrollToVerticalOffset(verticalOffset);
+        }
+
+        public int GetCurrtentPage()
+        {
+            //if scroll more than 90% of page height, add page index
+            int index = 1 + (int)((myScrollViewer.VerticalOffset + 0.1 * ZoomedPageHeight) / ZoomedPageHeight);
+
+            //AddLog($"GetCurrentPage(): index={index}, VerOffset={myScrollViewer.VerticalOffset}, ZoomedPageHeight={ZoomedPageHeight}");
+            AddLog($"GetCurrentPage(): index={index}, VerOffset={myScrollViewer.VerticalOffset}, ZoomedPageHeight={ZoomedPageHeight}");
+
+            return index;
+        }
+
+        private void CleanBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.LogCollection.Clear();
+        }
+
+        private void ZoomOut_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentZoomIndex > 0)
+            {
+                CurrentZoomIndex--;
+                SetZoom(CurrentZoomIndex);
+            }
+        }
+
+        private void ZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentZoomIndex < 2)
+            {
+                CurrentZoomIndex++;
+                SetZoom(CurrentZoomIndex);
+            }
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = "debug-Small.html";
+            LoadHtmlFile(filename);
         }
 
         private double bodyHeight;
@@ -261,6 +480,22 @@ namespace WpfIssue2751
             }
         }
 
+        public int PendingZoomIndex { get; set; }
+        public int ActualZoomIndex { get; set; }
+
+
+        private int currentZoomIndex;
+
+        public int CurrentZoomIndex
+        {
+            get { return currentZoomIndex; }
+            set
+            {
+                currentZoomIndex = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentZoomIndex)));
+            }
+        }
+
         private double zoomedPageWidth;
 
         public double ZoomedPageWidth
@@ -285,161 +520,18 @@ namespace WpfIssue2751
             }
         }
 
-        private void LoadHtmlFile(string filename)
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string myTitle;
+
+        public string MyTitle
         {
-            string html = File.ReadAllText(filename);
-            Dispatcher.Invoke(() =>
+            get { return myTitle; }
+            set
             {
-                browser.LoadHtml(html, "http://ReportingControl");
-
-            });
-        }
-
-        private void Btn1_Click(object sender, RoutedEventArgs e)
-        {
-            string filename = "debug.html";
-            LoadHtmlFile(filename);
-        }
-
-        private void DownBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (CurrentPage > 0)
-            {
-                CurrentPage--;
-                GotoPage(CurrentPage);
+                myTitle = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MyTitle)));
             }
-        }
-
-        private void UpBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (CurrentPage < (TotalPageCount-1))
-            {
-                CurrentPage++;
-                GotoPage(CurrentPage);
-            }
-
-        }
-
-        private void PART_VerticalScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            ;
-        }
-
-        private void PART_VerticalScrollBar_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
-        {
-            
-            ;
-        }
-
-        private void Zoom50_Click(object sender, RoutedEventArgs e)
-        {
-            SetZoom("50");
-        }
-
-        private void Zoom100_Click(object sender, RoutedEventArgs e)
-        {
-            SetZoom("100");
-
-        }
-
-        private void Zoom125_Click(object sender, RoutedEventArgs e)
-        {
-            SetZoom("125");
-
-        }
-
-        public double CurrentZoomPercent { get; set; }
-        public double CurrentZoomLevel { get; set; }
-
-        private double GetBrowserZoomPercent(string userPercent)
-        {
-            switch (userPercent)
-            {
-                case "50":
-                    return (ScrollViewHeight+10) / HtmlPageHeight;
-                case "125":
-                    return ScrollViewWidth*1.25 / HtmlPageWidth;
-                case "100":
-                default:
-                    return ScrollViewWidth / HtmlPageWidth;
-            }
-        }
-
-        private double GetBrowserZoomLevel(double zoomPercent)
-        {
-            double result = Math.Log(zoomPercent) / Math.Log(1.2);
-            //a little bigger
-            return Math.Ceiling(result * 1000) / 1000;
-        }
-
-        private void SetZoom(string userPercent)
-        {
-            AddLog($"SetZoom({userPercent})");
-
-            ////browser.Height = BodyHeight;
-            //browser.Height = ScreenHeight*9.103;
-            //browser.Width = BodyWidth;
-
-            //browser.InvalidateVisual();
-            int currentPageIndex = GetCurrtentPage();
-
-            CurrentZoomPercent = GetBrowserZoomPercent(userPercent);
-            CurrentZoomLevel = GetBrowserZoomLevel(CurrentZoomPercent);
-
-            ZoomedPageHeight = HtmlPageHeight * CurrentZoomPercent;
-            ZoomedPageWidth = HtmlPageWidth * CurrentZoomPercent;
-
-            Dispatcher.Invoke(() =>
-            {
-                AddLog($"browser.SetZoomLevel() - before");
-
-                browser.SetZoomLevel(CurrentZoomLevel);
-                //add a little more
-                //browser.Height = TotalPageCount * ZoomedPageHeight + 0.1;
-                browser.Height = ZoomedPageHeight * TotalPageCount;
-
-                //browser.Width = ZoomedPageWidth > ScrollViewWidth ? ZoomedPageWidth : ScrollViewWidth;
-                browser.Width = ZoomedPageWidth;
-
-                AddLog($"browser.InvalidateVisual() - before");
-
-                //update browser layout and render it
-                //browser.InvalidateMeasure();//This method calls InvalidateArrange internally.
-                browser.InvalidateVisual();//This method calls InvalidateArrange internally.
-                AddLog($"browser.InvalidateVisual() - after");
-            });
-
-            GotoPage(currentPageIndex);
-        }
-
-        public void GotoPage(int currentPageIndex)
-        {
-            AddLog($"GotoPage({currentPageIndex})");
-
-
-            int index = currentPageIndex;
-            if (index <= 1) { index = 1; }
-            if (index >= TotalPageCount) { index = TotalPageCount; }
-
-            //add a little more
-            double verticalOffset = (index - 1) * ZoomedPageHeight + 0.1 * CurrentZoomPercent;
-        }
-
-        public int GetCurrtentPage()
-        {
-            //if scroll more than 90% of page height, add page index
-            int index = 1 + (int)((myScrollViewer.VerticalOffset + 1 * ZoomedPageHeight) / ZoomedPageHeight);
-            return index;
-        }
-
-        private void MyScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
-        {
-            ;
-        }
-
-        private void CleanBtn_Click(object sender, RoutedEventArgs e)
-        {
-            this.LogCollection.Clear();
         }
     }
 }
