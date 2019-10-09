5dbd8ad9590ea0ef2a6e9d5f4c3887bd6b0844a5
DaZiYuan <mscoder@hotmail.com>
2019-10-09 11:51:59 +0800

    new demo

---
+++ winform.test/Form1.cs
@@ -0,0 +1,27 @@
+﻿using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform.test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var browser = new ChromiumWebBrowser("www.google.com")
+            {
+                Dock = DockStyle.Fill,
+            };
+            Controls.Add(browser);
+        }
+
+    }
+}
---
+++ winform.test/Program.cs
@@ -0,0 +1,49 @@
+using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform.test
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
+
+            //For Windows 7 and above, best to include relevant app.manifest entries as well
+            Cef.EnableHighDPISupport();
+
+            var settings = new CefSettings()
+            {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+            };
+
+            //Example of setting a command line argument
+            //Enables WebRTC
+            settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+
+            //Perform dependency check to make sure all relevant resources are in our output directory.
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+
+            Form1 f = new Form1();
+            f.Show();
+            Application.Run(new Form1());
+        }
+    }
+}
