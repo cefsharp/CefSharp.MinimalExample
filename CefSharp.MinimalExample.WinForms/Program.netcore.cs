173dd4e736c3f600b5462b823fb81b84667042b0
amaitland <amaitland@users.noreply.github.com>
2020-02-01 13:18:12 +1000

    Add Program.netcore.cs for each project
    Remove if def code so make example clearer

--- CefSharp.MinimalExample.WinForms/Program.netcore.cs
+++ CefSharp.MinimalExample.WinForms/Program.netcore.cs
@@ -17,10 +17,24 @@ namespace CefSharp.MinimalExample.WinForms
             //For Windows 7 and above, app.manifest entries will take precedences of this call
             Cef.EnableHighDPISupport();
 
+            //We are using our current exe as the BrowserSubProcess
+            //Multiple instances will be spawned to handle all the 
+            //Chromium proceses, render, gpu, network, plugin, etc.
+            var subProcessExe = new CefSharp.BrowserSubprocess.BrowserSubprocessExecutable();
+            var result = subProcessExe.Main(args);
+            if (result > 0)
+            {
+                return result;
+            }
+
+            //We use our current exe as the BrowserSubProcess
+            var exePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+
             var settings = new CefSettings()
             {
                 //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
-                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache"),
+                BrowserSubprocessPath = exePath
             };
 
             //Example of setting a command line argument
