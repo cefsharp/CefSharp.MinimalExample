da4e115a807b54f420c396362ee99f0a0a82039f
amaitland <amaitland@users.noreply.github.com>
2020-08-05 17:46:10 +1000

    Nuget Update - Add batch to update nuget packages
    First attempt, requires nuget.exe in folder directly above solution directory

---
+++ UpdateNugetPackages.bat
@@ -0,0 +1,9 @@
+SET cefsharpversion=84.3.10-pre
+
+..\nuget update CefSharp.MinimalExample.OffScreen\CefSharp.MinimalExample.OffScreen.csproj -Id CefSharp.OffScreen -Version %cefsharpversion%
+..\nuget update CefSharp.MinimalExample.WinForms\CefSharp.MinimalExample.WinForms.csproj -Id CefSharp.WinForms -Version %cefsharpversion%
+..\nuget update CefSharp.MinimalExample.Wpf\CefSharp.MinimalExample.Wpf.csproj -Id CefSharp.Wpf -Version %cefsharpversion%
+
+dotnet add CefSharp.MinimalExample.OffScreen\CefSharp.MinimalExample.OffScreen.netcore.csproj package CefSharp.OffScreen -v %cefsharpversion%
+dotnet add CefSharp.MinimalExample.WinForms\CefSharp.MinimalExample.WinForms.netcore.csproj package CefSharp.WinForms -v %cefsharpversion%
+dotnet add CefSharp.MinimalExample.Wpf\CefSharp.MinimalExample.Wpf.netcore.csproj package CefSharp.Wpf -v %cefsharpversion%
