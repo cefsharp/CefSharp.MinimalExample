d040f8b3aef9a306f48d6d6e296726b9de1e0bdf
amaitland <amaitland@users.noreply.github.com>
2020-09-09 17:51:12 +1000

    WPF/WinForms - Improve WebRTC Example and links

--- CefSharp.MinimalExample.WinForms/Program.cs
+++ CefSharp.MinimalExample.WinForms/Program.cs
@@ -44,7 +44,16 @@ namespace CefSharp.MinimalExample.WinForms
 
             //Example of setting a command line argument
             //Enables WebRTC
+            // - CEF Doesn't currently support permissions on a per browser basis see https://bitbucket.org/chromiumembedded/cef/issues/2582/allow-run-time-handling-of-media-access
+            // - CEF Doesn't currently support displaying a UI for media access permissions
+            //
+            //NOTE: WebRTC Device Id's aren't persisted as they are in Chrome see https://bitbucket.org/chromiumembedded/cef/issues/2064/persist-webrtc-deviceids-across-restart
             settings.CefCommandLineArgs.Add("enable-media-stream");
+            //https://peter.sh/experiments/chromium-command-line-switches/#use-fake-ui-for-media-stream
+            settings.CefCommandLineArgs.Add("use-fake-ui-for-media-stream");
+            //For screen sharing add (see https://bitbucket.org/chromiumembedded/cef/issues/2582/allow-run-time-handling-of-media-access#comment-58677180)
+            settings.CefCommandLineArgs.Add("enable-usermedia-screen-capturing");
+
 
             //Perform dependency check to make sure all relevant resources are in our output directory.
             Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
