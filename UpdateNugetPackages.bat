829fbec6d1180b4ade6cb81a8ff4666349df0f69
Alex Maitland <amaitland@users.noreply.github.com>
2020-10-25 13:34:11 +1000

    Upgrade to 85.3.130 (#115)
    * Upgrade to 85.3.130

--- UpdateNugetPackages.bat
+++ UpdateNugetPackages.bat
@@ -1,4 +1,6 @@
-SET cefsharpversion=84.4.10
+SET cefsharpversion=85.3.130
+
+..\nuget restore CefSharp.MinimalExample.sln
 
 ..\nuget update CefSharp.MinimalExample.OffScreen\CefSharp.MinimalExample.OffScreen.csproj -Id CefSharp.OffScreen -Version %cefsharpversion%
 ..\nuget update CefSharp.MinimalExample.WinForms\CefSharp.MinimalExample.WinForms.csproj -Id CefSharp.WinForms -Version %cefsharpversion%
@@ -7,3 +9,5 @@ SET cefsharpversion=84.4.10
 dotnet add CefSharp.MinimalExample.OffScreen\CefSharp.MinimalExample.OffScreen.netcore.csproj package CefSharp.OffScreen -v %cefsharpversion%
 dotnet add CefSharp.MinimalExample.WinForms\CefSharp.MinimalExample.WinForms.netcore.csproj package CefSharp.WinForms -v %cefsharpversion%
 dotnet add CefSharp.MinimalExample.Wpf\CefSharp.MinimalExample.Wpf.netcore.csproj package CefSharp.Wpf -v %cefsharpversion%
+
+pause
