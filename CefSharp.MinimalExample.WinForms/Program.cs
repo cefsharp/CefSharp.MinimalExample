ad7be153a6f9d5f02b45ba6ff8ed73806d9c6470
Alex Maitland <amaitland@users.noreply.github.com>
2021-01-09 14:36:05 +1000

    Upgrade to 87.1.12-CI3846
    - Upgrade to Net Core 3.1 (Required to use new Net Core packages)
    - Migrate to NetCore specific pages

--- CefSharp.MinimalExample.WinForms/Program.cs
+++ CefSharp.MinimalExample.WinForms/Program.cs
@@ -17,30 +17,11 @@ namespace CefSharp.MinimalExample.WinForms
             //For Windows 7 and above, best to include relevant app.manifest entries as well
             Cef.EnableHighDPISupport();
 
-#if NETCOREAPP
-            //We are using our current exe as the BrowserSubProcess
-            //Multiple instances will be spawned to handle all the 
-            //Chromium proceses, render, gpu, network, plugin, etc.
-            var subProcessExe = new CefSharp.BrowserSubprocess.BrowserSubprocessExecutable();
-            var result = subProcessExe.Main(args);
-            if (result > 0)
-            {
-                return result;
-            }
-#endif
-
             var settings = new CefSettings()
             {
                 //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
                 CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
             };
-
-#if NETCOREAPP
-            //We use our Applications exe as the BrowserSubProcess, multiple copies
-            //will be spawned
-            var exePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
-            settings.BrowserSubprocessPath = exePath;
-#endif
 
             //Example of setting a command line argument
             //Enables WebRTC
@@ -54,9 +35,15 @@ namespace CefSharp.MinimalExample.WinForms
             //For screen sharing add (see https://bitbucket.org/chromiumembedded/cef/issues/2582/allow-run-time-handling-of-media-access#comment-58677180)
             settings.CefCommandLineArgs.Add("enable-usermedia-screen-capturing");
 
+            var dependencyCheck = true;
+
+#if NETCOREAPP
+            //This should be fixed shortly.
+            dependencyCheck = false;
+#endif
 
             //Perform dependency check to make sure all relevant resources are in our output directory.
-            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+            Cef.Initialize(settings, performDependencyCheck: dependencyCheck, browserProcessHandler: null);
 
             var browser = new BrowserForm();
             Application.Run(browser);
