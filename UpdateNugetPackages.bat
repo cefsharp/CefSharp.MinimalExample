90d5611e9ad13b6b1c5d5e5bb691f06c406ed760
Alex Maitland <amaitland@users.noreply.github.com>
2022-03-02 10:10:09 +1000

    Net 4.7.2 - Add WPF Example

--- UpdateNugetPackages.bat
+++ UpdateNugetPackages.bat
@@ -6,9 +6,9 @@ SET cefsharpversion=98.1.210
 ..\nuget update CefSharp.MinimalExample.WinForms\CefSharp.MinimalExample.WinForms.csproj -Id CefSharp.WinForms -Version %cefsharpversion%
 ..\nuget update CefSharp.MinimalExample.Wpf\CefSharp.MinimalExample.Wpf.csproj -Id CefSharp.Wpf -Version %cefsharpversion%
 
-rem #dotnet add CefSharp.MinimalExample.OffScreen\CefSharp.MinimalExample.OffScreen.netcore.csproj package CefSharp.OffScreen.NetCore -v %cefsharpversion%
+rem #dotnet add CefSharp.MinimalExample.OffScreen\CefSharp.MinimalExample.OffScreen.net472.csproj package CefSharp.OffScreen -v %cefsharpversion%
 dotnet add CefSharp.MinimalExample.WinForms\CefSharp.MinimalExample.WinForms.net472.csproj package CefSharp.WinForms -v %cefsharpversion%
-rem dotnet add CefSharp.MinimalExample.Wpf\CefSharp.MinimalExample.Wpf.netcore.csproj package CefSharp.Wpf.NetCore -v %cefsharpversion%
+dotnet add CefSharp.MinimalExample.Wpf\CefSharp.MinimalExample.Wpf.net472.csproj package CefSharp.Wpf -v %cefsharpversion%
 
 dotnet add CefSharp.MinimalExample.OffScreen\CefSharp.MinimalExample.OffScreen.netcore.csproj package CefSharp.OffScreen.NetCore -v %cefsharpversion%
 dotnet add CefSharp.MinimalExample.WinForms\CefSharp.MinimalExample.WinForms.netcore.csproj package CefSharp.WinForms.NetCore -v %cefsharpversion%
