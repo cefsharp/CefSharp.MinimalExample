5cc2a0ca3fe76b2e3814304dfc0d3aa9ad368536
Alex Maitland <amaitland@users.noreply.github.com>
2023-01-03 12:13:10 +1000

    WinForms - Load invalid Urls in google search

--- CefSharp.MinimalExample.WinForms/BrowserForm.cs
+++ CefSharp.MinimalExample.WinForms/BrowserForm.cs
@@ -6,6 +6,7 @@ using CefSharp.DevTools.IO;
 using CefSharp.MinimalExample.WinForms.Controls;
 using CefSharp.WinForms;
 using System;
+using System.Net;
 using System.Windows.Forms;
 
 namespace CefSharp.MinimalExample.WinForms
@@ -185,12 +186,66 @@ namespace CefSharp.MinimalExample.WinForms
             LoadUrl(urlTextBox.Text);
         }
 
-        private void LoadUrl(string url)
+        private void LoadUrl(string urlString)
         {
-            if (Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+            // No action unless the user types in some sort of url
+            if (string.IsNullOrEmpty(urlString))
             {
-                browser.Load(url);
+                return;
             }
+
+            Uri url;
+
+            var success = Uri.TryCreate(urlString, UriKind.RelativeOrAbsolute, out url);
+
+            // Basic parsing was a success, now we need to perform additional checks
+            if (success)
+            {
+                // Load absolute urls directly.
+                // You may wish to validate the scheme is http/https
+                // e.g. url.Scheme == Uri.UriSchemeHttp || url.Scheme == Uri.UriSchemeHttps
+                if (url.IsAbsoluteUri)
+                {
+                    browser.LoadUrl(urlString);
+
+                    return;
+                }
+
+                // Relative Url
+                // We'll do some additional checks to see if we can load the Url
+                // or if we pass the url off to the search engine
+                var hostNameType = Uri.CheckHostName(urlString);
+
+                if (hostNameType == UriHostNameType.IPv4 || hostNameType == UriHostNameType.IPv6)
+                {
+                    browser.LoadUrl(urlString);
+
+                    return;
+                }
+
+                if (hostNameType == UriHostNameType.Dns)
+                {
+                    try
+                    {
+                        var hostEntry = Dns.GetHostEntry(urlString);
+                        if (hostEntry.AddressList.Length > 0)
+                        {
+                            browser.LoadUrl(urlString);
+
+                            return;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        // Failed to resolve the host
+                    }
+                }
+            }
+
+            // Failed parsing load urlString is a search engine
+            var searchUrl = "https://www.google.com/search?q=" + Uri.EscapeDataString(urlString);
+
+            browser.LoadUrl(searchUrl);
         }
 
         private void ShowDevToolsMenuItemClick(object sender, EventArgs e)
