3384447bba2448a3d89a78259b9f2fa47bb5f1bf
Alex Maitland <amaitland@users.noreply.github.com>
2023-02-26 18:39:30 +1000

    OffScreen - Add net472 csproj

--- UpdateNugetPackages.bat
+++ UpdateNugetPackages.bat
@@ -6,7 +6,7 @@ SET cefsharpversion=110.0.280
 ..\nuget update CefSharp.MinimalExample.WinForms\CefSharp.MinimalExample.WinForms.csproj -Id CefSharp.WinForms -Version %cefsharpversion%
 ..\nuget update CefSharp.MinimalExample.Wpf\CefSharp.MinimalExample.Wpf.csproj -Id CefSharp.Wpf -Version %cefsharpversion%
 
-rem #dotnet add CefSharp.MinimalExample.OffScreen\CefSharp.MinimalExample.OffScreen.net472.csproj package CefSharp.OffScreen -v %cefsharpversion%
+dotnet add CefSharp.MinimalExample.OffScreen\CefSharp.MinimalExample.OffScreen.net472.csproj package CefSharp.OffScreen -v %cefsharpversion%
 dotnet add CefSharp.MinimalExample.WinForms\CefSharp.MinimalExample.WinForms.net472.csproj package CefSharp.WinForms -v %cefsharpversion%
 dotnet add CefSharp.MinimalExample.Wpf\CefSharp.MinimalExample.Wpf.net472.csproj package CefSharp.Wpf -v %cefsharpversion%
 
