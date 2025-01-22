2d73345d8f8b6d05cbf84ed2624c411a28fd591f
amaitland <307872+amaitland@users.noreply.github.com>
2025-01-22 17:01:36 +1000

    Wpf HwndHost - Initial Import

---
+++ CefSharp.MinimalExample.Wpf.HwndHost/Converter/TitleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CefSharp.MinimalExample.Wpf.HwndHost.Converter
+{
+    public class TitleConverter : IValueConverter
+    {
+#if DEBUG
+        private const string Build = "Debug";
+#else
+        private const string Build = "Release";
+#endif
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "CefSharp.MinimalExample.Wpf.HwndHost (" + Build + ") - " + (value ?? "No Title Specified");
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ CefSharp.MinimalExample.Wpf.HwndHost/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CefSharp.MinimalExample.Wpf.HwndHost
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
